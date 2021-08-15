f5fc246a905b63f9a1e2b0216f3b6b7c3b232635
Zia Rehman <iamziarehman@gmail.com>
2021-08-15 01:08:15 +0100

    Created basic game that can play one round

--- resources/js/game.js
+++ resources/js/game.js
@@ -6,10 +6,44 @@ function computerPlay() {
 
     switch (choice) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         default:
-            return "Scissors";
+            return "scissors";
     }
 }
+
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection){
+        return "Tie!"
+    } else if (playerSelection === "rock"){
+        if (computerSelection =="paper") {
+            return "You Lose! Paper beats Rock";
+        } else {
+            return "You Win! Rock beats Scissors";
+        }
+    } else if (playerSelection === "paper"){
+        if (computerSelection === "scissors"){
+            return "You Lose! Scissors beats Paper!";
+        } else {
+            return "You Win! Paper beats Rock!"
+        }
+    } else if (playerSelection ==="scissors"){
+        if (computerSelection === "rock") {
+            return "You Lose! Rock beats Scissors!";
+        } else {
+            return "You Win! Scissors beats Paper!";
+        }
+    } else {
+        return "Invalid Input";
+    }
+}
+
+
+let playerSelection = prompt("Rock, Paper, Scissors? (input below)");
+computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
